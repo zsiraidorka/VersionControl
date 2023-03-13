7e435cea15fcf58aa09cbf9dd1bc1e71a97366c8
Dorka Zsirai <dorka.zsirai@stud.uni-corvinus.hu>
2023-03-13 21:21:04 +0100

    negyesig

--- 4.het/4.het/Form1.cs
+++ 4.het/4.het/Form1.cs
@@ -12,9 +12,17 @@ namespace _4.het
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
