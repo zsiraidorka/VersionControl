a65e3c0dde5984294905f4bd8a68ef882d72337c
Dorka Zsirai <dorka.zsirai@stud.uni-corvinus.hu>
2023-03-28 08:50:51 +0200

    harom

--- gyak5/gyak5/Form1.cs
+++ gyak5/gyak5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak5.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,23 @@ namespace gyak5
         public Form1()
         {
             InitializeComponent();
+            GetRates();
+        }
+
+        private void GetRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+          
         }
     }
 }
